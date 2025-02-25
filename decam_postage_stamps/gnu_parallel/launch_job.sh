32c586df27714d4b6c0c1d2e2b29ed8f8ef52029
Rongpu Zhou <rongpu.zhou@gmail.com>
2025-02-25 09:05:43 -0800

    .

--- decam_postage_stamps/gnu_parallel/launch_job.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --qos=regular
-#SBATCH --nodes=6
-#SBATCH --ntasks-per-node 1
-#SBATCH --constraint=haswell
-#SBATCH --time=24:00:00
-#SBATCH --account=desi
-
-srun --wait=0 payload.sh tasks.txt
